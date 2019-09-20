cd484fcdc9d4ef1c09d5f17612dee53e4a3ff8a1
Alicia Key <alicia.key@nrel.gov>
2019-09-20 10:10:46 -0600

    Issue #6
    Take out the MPI part of the code because that won't necessarily be avaialable in the production testing environments.
    
    Also, do not optimize the system, because (at least for now) that could slow doen the tests too much.

--- wisdem/assemblies/land_based/run_land_based_noGenerator_noBOS_lcoe.py
+++ wisdem/assemblies/land_based/run_land_based_noGenerator_noBOS_lcoe.py
@@ -13,8 +13,6 @@ from .land_based_noGenerator_noBOS_lcoe import Init_LandBasedAssembly, LandBased
 
 
 if __name__ == "__main__":
-    optFlag = False
-
     # Reference rotor design
     fname_schema = "../../rotorse/turbine_inputs/IEAontology_schema.yaml"
     fname_input = "../../rotorse/turbine_inputs/nrel5mw_mod_update.yaml"
@@ -32,64 +30,8 @@ if __name__ == "__main__":
     # Initialize tower design
     Nsection_Tow = 6
 
-    # Initialize OpenMDAO problem and FloatingSE Group
-    if MPI:
-        num_par_fd = MPI.COMM_WORLD.Get_size()
-        prob = Problem(model=Group(num_par_fd=num_par_fd))
-        prob.model.approx_totals(method='fd')
-        prob.model.add_subsystem('comp',
-                                 LandBasedTurbine(RefBlade=blade, Nsection_Tow=Nsection_Tow, VerbosityCosts=True),
-                                 promotes=['*'])
-    else:
-        prob = Problem()
-        prob.model = LandBasedTurbine(RefBlade=blade, Nsection_Tow=Nsection_Tow, VerbosityCosts=True)
-
-    if optFlag:
-        # --- Solver ---
-        prob.driver = ScipyOptimizeDriver()
-        prob.driver.options['optimizer'] = 'SLSQP'
-        prob.driver.options['tol'] = 1.e-6
-        prob.driver.options['maxiter'] = 100
-        # ----------------------
-
-        # --- Objective ---
-        prob.model.add_objective('lcoe')
-        # ----------------------
-
-        # --- Design Variables ---
-        indices_no_root = range(2, refBlade.NINPUT)
-        indices_no_root_no_tip = range(2, refBlade.NINPUT - 1)
-        prob.model.add_design_var('chord_in', indices=indices_no_root_no_tip, lower=0.5, upper=7.0)
-        prob.model.add_design_var('theta_in', indices=indices_no_root, lower=-5.0, upper=20.0)
-        prob.model.add_design_var('sparT_in', indices=indices_no_root_no_tip, lower=0.001, upper=0.200)
-        prob.model.add_design_var('control_tsr', lower=6.000, upper=11.00)
-        prob.model.add_design_var('tower_section_height', lower=5.0, upper=80.0)
-        prob.model.add_design_var('tower_outer_diameter', lower=3.87, upper=30.0)
-        prob.model.add_design_var('tower_wall_thickness', lower=4e-3, upper=2e-1)
-        # ----------------------
-
-        # --- Constraints ---
-        # Rotor
-        prob.model.add_constraint('tip_deflection_ratio', upper=1.0)
-        # Tower
-        prob.model.add_constraint('tow.height_constraint', lower=-1e-2, upper=1.e-2)
-        prob.model.add_constraint('tow.post.stress', upper=1.0)
-        prob.model.add_constraint('tow.post.global_buckling', upper=1.0)
-        prob.model.add_constraint('tow.post.shell_buckling', upper=1.0)
-        prob.model.add_constraint('tow.weldability', upper=0.0)
-        prob.model.add_constraint('tow.manufacturability', lower=0.0)
-        prob.model.add_constraint('frequencyNP_margin', upper=0.)
-        prob.model.add_constraint('frequency1P_margin', upper=0.)
-        prob.model.add_constraint('ground_clearance', lower=20.0)
-        # ----------------------
-
-        # --- Recorder ---
-        prob.driver.add_recorder(SqliteRecorder('log_opt.sql'))
-        prob.driver.recording_options['includes'] = ['AEP', 'rc.total_blade_cost', 'lcoe', 'tip_deflection_ratio']
-        prob.driver.recording_options['record_objectives'] = True
-        prob.driver.recording_options['record_constraints'] = True
-        prob.driver.recording_options['record_desvars'] = True
-        # ----------------------
+    prob = Problem()
+    prob.model = LandBasedTurbine(RefBlade=blade, Nsection_Tow=Nsection_Tow, VerbosityCosts=True)
 
     prob.setup(check=True)
 
